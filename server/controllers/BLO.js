54027cb1384ecf6841d230533605052f32140d64
bhavyakedar <58164903+bhavyakedar@users.noreply.github.com>
2022-04-12 20:01:31 +0530

    BLO Controllers

--- server/controllers/BLO.js
+++ server/controllers/BLO.js
@@ -1,5 +1,53 @@
-const { addDoc } = require("firebase/firestore");
+const { addDoc, getDocs, collection, where, updateDoc, doc } = require("firebase/firestore");
 const { db } = require("../firebase");
 
-exports.getVotersList = async (req, res) => {};
-exports.verifyVoter = async (req, res) => {};
+exports.getVotersList = async (req, res) => {
+    try{
+        const docSnap = await getDocs(collection(db, "Voters"), where("area", "==", req.params.area));
+
+        const data = [];
+
+        docSnap.forEach(doc => {
+            data.push({id: doc.id, ...doc.data()})
+        })        
+        
+        return res.status(200).json({
+            success: true,
+            msg: "Voters list fetched successfully",
+            data
+        })
+    }   
+    catch(error)
+    {
+        console.log(error);
+
+        return res.status(500).json({
+            success:false,
+            error,
+            msg: "Error in fetching list"
+        })
+    }
+};
+exports.verifyVoter = async (req, res) => {
+    try{
+        await updateDoc(doc(db, "Voters", req.body.userId),{
+            verified: true,
+            verifiedBy: req.params.id
+        });   
+        
+        return res.status(200).json({
+            success: true,
+            msg: "Voter verified successfully"
+        })
+    }   
+    catch(error)
+    {
+        console.log(error);
+
+        return res.status(500).json({
+            success:false,
+            error,
+            msg: "Server error"
+        })
+    }
+};
